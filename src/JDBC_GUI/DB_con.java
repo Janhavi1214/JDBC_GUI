4c942ce667de3836a445cf580caac31151bed5f0
Janhavi1214 <1680.janhavi@gmail.com>
2026-05-26 15:57:32 +0530

    Update DB_con.java

--- src/JDBC_GUI/DB_con.java
+++ src/JDBC_GUI/DB_con.java
@@ -8,7 +8,7 @@ public class DB_con {
 		
 		String URL = "jdbc:mysql://localhost:3306/";
 		String user = "root";
-		String password = "janhavi@2004";
+		String password = "password";
 		
 		String dbName = "my_new_db";
 		
